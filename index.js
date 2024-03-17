fe7c48f3c7f4f2f5e6382b372d488ff43af7ef1d
Murilo Andrade <muriloandra@icloud.com>
2024-03-17 18:59:10 -0400

    perf improve performance in a index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 let openP = document.querySelector(".explorer__btn_form");
 let closeP = document.querySelector(".form__button-esc");
-let formClose = document.querySelector(".form__edit");
+let formClose = document.querySelector(".form__close");
+let openPage = document.querySelector(".page");
+let submiTform = document.querySelector(".form__edit_change_save");
+
+// Funcao para abertura e fechamento do formulario com opacidade da pagina.
 
 function openForm() {
   formClose.style.display = "block";
+  openPage.style.opacity = "0.4";
 }
 
 function closeForm() {
   formClose.style.display = "none";
 }
 
-openP.addEventListener("click", openForm);
+function closePage() {
+  openPage.style.opacity = "1";
+}
 
+openP.addEventListener("click", openForm);
 closeP.addEventListener("click", closeForm);
+submiTform.addEventListener("click", closePage);
+closeP.addEventListener("click", closePage);
+
+// Funcao para alterar os botoes de curtida das fotos.
 
 var buttons = document.querySelectorAll(".local__img_btn_heart");
 
@@ -28,9 +40,33 @@ buttons.forEach(function (button) {
   });
 });
 
-// function changeBackground() {
-//   var element = document.getElementById("btn");
-//   element.classList.toggle("local__img_btn_heart");
-//   onclick()changeBackground
-//   id=""
-// }
+let formElment = document.querySelector(".form__edit_change");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+  // Explicaremos em mais detalhes posteriormente.
+
+  // Vamos encontrar os campos de formulário do DOM
+  let nameInput = document.querySelector("#name");
+  let jobInput = document.querySelector("#author");
+  let formClean = document.querySelector(".form__close-clean");
+
+  // Pegue os valores de cada campo do valor da propriedade correspondente
+  let name = nameInput.value;
+  let job = jobInput.value;
+
+  // Selecione os elementos aos quais os valores dos campos serão inseridos
+  let nameDisplay = document.querySelector("#change");
+  let jobDisplay = document.querySelector("#switch");
+
+  // Insira novos valores usando a propriedade textContent
+  nameDisplay.textContent = name;
+  jobDisplay.textContent = job;
+  formClean.style.display = "none";
+}
+
+// Conecte o handler ao formulário:
+// ele vai observar o evento de submit
+
+formElment.addEventListener("submit", handleProfileFormSubmit);
