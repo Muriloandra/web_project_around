7fe19bedd1deaee08f7e2d1c145454162377da7e
Murilo Andrade <muriloandra@icloud.com>
2024-03-18 20:06:36 -0400

    fixed index.js and blocks/forms

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let openP = document.querySelector(".explorer__btn-form");
-let closeP = document.querySelector(".form__close_button-esc");
-let formClose = document.querySelector(".form__close");
+let closeP = document.querySelector(".form__edit_container_button-esc");
+let formClose = document.querySelector(".form__edit");
 let openPage = document.querySelector(".page");
-let submiTform = document.querySelector(".form__close_edit-change-save");
+let submiTform = document.querySelector(".form__edit_change_save");
 
 // Funcao para abertura e fechamento do formulario com opacidade da pagina.
 
@@ -40,14 +40,14 @@ buttons.forEach(function (button) {
   });
 });
 
-let formElment = document.querySelector(".form__close_edit-change");
+let formElment = document.querySelector(".form__edit_change");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
 
   let nameInput = document.querySelector("#name");
   let jobInput = document.querySelector("#author");
-  let formClean = document.querySelector(".form__close");
+  let formClean = document.querySelector(".form__edit");
 
   let name = nameInput.value;
   let job = jobInput.value;
