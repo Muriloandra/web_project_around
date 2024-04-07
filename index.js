7804c1286346c2c413a4a075829e2261b6fbf18f
Murilo Andrade <muriloandra@icloud.com>
2024-04-06 21:55:30 -0400

    add block forming, feat a new form in javascript

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 let openP = document.querySelector(".explorer__btn-form");
 let closeP = document.querySelector(".form__edit-container-button-esc");
+let closeBut = document.querySelector(".forming__edit-container-button-esc");
 let formClose = document.querySelector(".form");
 let openPage = document.querySelector(".page");
 let submiTform = document.querySelector(".form__edit-change-save");
-
+let openBut = document.querySelector(".explorer__btn-insert");
+let closeB = document.querySelector(".forming");
 // Funcao para abertura e fechamento do formulario com opacidade da pagina.
 
 function openForm() {
   formClose.style.display = "block";
   openPage.style.opacity = "0.4";
+  openBut.disabled = true;
+}
+
+function openImg() {
+  closeB.style.display = "block";
+  openPage.style.opacity = "0.4";
+}
+function closeImg() {
+  closeB.style.display = "none";
 }
 
 function closeForm() {
   formClose.style.display = "none";
+  openBut.disabled = false;
 }
 
 function closePage() {
   openPage.style.opacity = "1";
 }
 
+openBut.addEventListener("click", openImg);
+closeBut.addEventListener("click", closeImg);
+closeBut.addEventListener("click", closePage);
 openP.addEventListener("click", openForm);
 closeP.addEventListener("click", closeForm);
-submiTform.addEventListener("click", closePage);
 closeP.addEventListener("click", closePage);
+submiTform.addEventListener("click", closePage);
 
 // Funcao para alterar os botoes de curtida das fotos.
 
@@ -58,6 +73,29 @@ function handleProfileFormSubmit(evt) {
   nameDisplay.textContent = name;
   jobDisplay.textContent = job;
   formClean.style.display = "none";
+  openBut.disabled = false;
 }
 
 formElment.addEventListener("submit", handleProfileFormSubmit);
+
+// let formingElement = document.querySelector(".forming__edit-change");
+
+// function handleProfileFormingSubmit(evt) {
+//   evt.preventDefault();
+
+//   let titleInput = document.querySelector("#title");
+//   let linkInput = document.querySelector("#link");
+//   let formingClean = document.querySelector(".forming-clean");
+
+//   let title = titleInput.value;
+//   let link = linkInput.value;
+
+//   // let nameDisplay = document.querySelector("#change");
+//   // let jobDisplay = document.querySelector("#switch");
+
+//   // titleDisplay.textContent = title;
+//   // linkDisplay.textContent = link;
+//   formingClean.style.display = "none";
+// }
+
+// formingElement.addEventListener("submit", handleProfileFormingSubmit);
